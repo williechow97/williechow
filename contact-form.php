fc8f0b559b8acde66bea8eb519bc84d516b7604a
williechow97 <willie.chow01@gmail.com>
2019-09-22 18:51:31 -0700

    add functionality to contact portion

---
+++ contact-form.php
@@ -0,0 +1,16 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email =$_POST['email'];
+	$message=$_POST['message'];
+
+	$email_from = 'willie.chow01@gmail.com';
+	$email_subject = "Contact from Personal Website";
+	$email_body = "User Name: $name.\n".
+					"User Email: $visitor_email.\n".
+					"User message: $message.\n";
+
+	$to= "willie.chow01@gmail.com";
+	$headers="Reply To: $visitor_email\r\n";
+	mail($to,email_subject,$email_body,$headers);
+	header("Location: index.html");
+?>
