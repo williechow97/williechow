429741fb40066292247166e2b81b6a25b7d3ae54
williechow97 <willie.chow01@gmail.com>
2019-09-22 18:59:24 -0700

    update

--- contact-form.php
+++ contact-form.php
@@ -10,6 +10,7 @@
 					"User message: $message.\n";
 
 	$to= "willie.chow01@gmail.com";
+	$headers="From:$email_from \r\n";
 	$headers="Reply To: $visitor_email\r\n";
 	mail($to,email_subject,$email_body,$headers);
 	header("Location: index.html");
